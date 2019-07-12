5f9097d9bfb6c6b3d6e3e0efd05686458bbb2141
aditya <pateladi28@gmail.com>
2019-07-12 23:02:43 +0530

    MenQr

---
+++ app/src/main/java/com/patel/aditya/menqr/MainActivity.java
@@ -0,0 +1,13 @@
+package com.patel.aditya.menqr;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.options_layout);
+    }
+}
